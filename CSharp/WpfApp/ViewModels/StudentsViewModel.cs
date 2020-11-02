8fbb25c056817ca89dae6837d2198cbed17ea3b4
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-02 12:06:11 +0100

    Students

---
+++ CSharp/WpfApp/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.ViewModels
+{
+    public class StudentsViewModel
+    {
+        //public ICollection<Student> Students { get; } = new List<Student>() { new Student { FirstName = "Ewa", LastName = "Ewowska" }, new Student { FirstName = "Piotr", LastName = "Piotrowski" } };
+
+        public ICollection<Student> Students { get; }
+        public StudentsViewModel() {
+            Students = new List<Student>() { new Student { FirstName = "Ewa", LastName = "Ewowska" }, new Student { FirstName = "Piotr", LastName = "Piotrowski" } };
+        }
+    }
+}
