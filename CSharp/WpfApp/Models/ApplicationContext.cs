c4545d306d461bca08f3ab1497d6e0b6912551e0
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-03 14:27:48 +0100

    Singleton

---
+++ CSharp/WpfApp/Models/ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public class ApplicationContext
+    {
+        private ApplicationContext()
+        {
+
+        }
+
+        private static ApplicationContext _instance;
+
+        private object _locker = new object();
+
+        public ApplicationContext GetInstance()
+        {
+            lock (_locker)
+            {
+               if (_instance == null)
+                    _instance = new ApplicationContext();
+                return _instance;
+            }
+        }
+
+        //public ApplicationContext Instance { get; } = new ApplicationContext();
+    }
+}
